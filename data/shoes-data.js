ed3962a0f5203efc1216245e69c183945cdd8495
Marie Blakely <marieblakely@myyahoo.com>
2024-04-10 08:41:53 -0400

    complete array of objects data

---
+++ data/shoes-data.js
@@ -0,0 +1,9 @@
+const shoes = [
+  {brand: 'Nike', sneaker: true, _id: 125223},
+  {brand: 'Adidas', sneaker: true, _id: 127904},
+  {brand: 'Betsey Johnson', done: false, _id: 139608},
+]
+
+export { 
+	shoes
+}
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express()
 
 // configure the app (app.set)
 
+app.set('view engine', 'ejs')
 
 
 // mount Middleware (app.use)
@@ -21,7 +22,7 @@ app.get('/', function(req, res){
 })
 
 app.get('/home', function(req, res){
-  res.send('<h1>Home Page</h1>')
+  res.render('home')
 })
 // tell the app to listen on port 3000
 
